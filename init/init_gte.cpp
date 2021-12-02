a73c5cb89c12d819e2199ef0c5ef93943e4664f2
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-12-02 17:31:34 -0500

    gte-common: Updating init file for Q
    - Adapting matisse/Mille init file for GT device.
    
    - gtesqltespr
    - gtelwifiue
    
    S3NEO/android_device_samsung_matisse-common@0a3081f5e7a5e65700c73e2d7ea7198d80f5d11c
    S3NEO/android_device_samsung_matisse-common@3846f768e21761a72ae23a0e9beadb1a1c2150ea
    S3NEO/android_device_samsung_matisse-common@f628897d7dda1b9b534f9ad62ca023c9cafc220c
    S3NEO/android_device_samsung_matisse-common@72e859db79f78b01e39ef70cccbd91b349627454
    S3NEO/android_device_samsung_matisse-common@519686f042947625d75be27934f732eaaf445718
    S3NEO/android_device_samsung_matisse-common@4c1d2b675042c7bdffbfef5752494c5d064c5cc2
    S3NEO/android_device_samsung_matisse-common@214c6d7fa301148443ee4025bce862d792b94d3f
    S3NEO/android_device_samsung_matisse-common@4da1994bed50643630dfedee1d7f204c859a9f5e
    S3NEO/android_device_samsung_matisse-common@eae34cf5413d4ade9646abbc31e1600ec013a977
    S3NEO/android_device_samsung_matisse-common@6fcab48f8f5e3d4c2a6ceeee595b7bd09918581f
    S3NEO/android_device_samsung_matisse-common@cb3d139f01062519dad5831d2f3ef927324c6393
    S3NEO/android_device_samsung_matisse-common@1a0dfc200196acf3919f1c20dbf93ed07d78367c
    S3NEO/android_device_samsung_matisse-common@f5cc2544f6a30eb190afffb5ef6a2dc64d8f8dd3
    S3NEO/android_device_samsung_matisse-common@db3862016d5fe7fdd35409305f3c31ac065b7e08
    S3NEO/android_device_samsung_matisse-common@f000b5ee12d384663dc395f1ee637db8b3378f25
    S3NEO/android_device_samsung_matisse-common@9aba9c0fdb690a1fd5d08a61bdd0fb5d9b130618
    S3NEO/android_device_samsung_matisse-common@b87c4937ef1c304d7a046fdd3f875548a6bfcc76
    S3NEO/android_device_samsung_matisse-common@ac7fff4b8fbad0f55bc771b7da68ef5c3d53eeda
    S3NEO/android_device_samsung_matisse-common@fd8ff43a3a7fc958ece1c5367a1dc5de1277d349
    S3NEO/android_device_samsung_matisse-common@0dab3f0820e30b70d6d714ea7aa13ac647602daf
    S3NEO/android_device_samsung_matisse-common@8b09473b423eae3cfbe2b8055920463998e7fe21
    S3NEO/android_device_samsung_matisse-common@551d7f03d62dcb8998c790551bab3134d030df75
    S3NEO/android_device_samsung_matisse-common@acc9de40c5b17566b25402e8c84c1824f842560c
    S3NEO/android_device_samsung_matisse-common@ac7fff4b8fbad0f55bc771b7da68ef5c3d53eeda
    S3NEO/android_device_samsung_matisse-common@fb3286f98ef8a7b1e47b90a6824b90719b4edabc
    S3NEO/android_device_samsung_matisse-common@69845ae42bf8ff01e435a9241db8809f4003b866

--- init/init_gte.cpp
+++ init/init_gte.cpp
@@ -1,18 +1,19 @@
 /*
-   Copyright (c) 2017, The Linux Foundation. All rights reserved.
+   Copyright (c) 2016, The Linux Foundation. All rights reserved.
+   Copyright (c) 2017-2020, The LineageOS Project. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
-	* Redistributions of source code must retain the above copyright
-	  notice, this list of conditions and the following disclaimer.
-	* Redistributions in binary form must reproduce the above
-	  copyright notice, this list of conditions and the following
-	  disclaimer in the documentation and/or other materials provided
-	  with the distribution.
-	* Neither the name of The Linux Foundation nor the names of its
-	  contributors may be used to endorse or promote products derived
-	  from this software without specific prior written permission.
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
 
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
@@ -27,162 +28,42 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <android-base/file.h>
-#include <android-base/strings.h>
-
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
-
-#define SIMSLOT_FILE "/proc/simslot_count"
-
-#include <android-base/properties.h>
 #include <android-base/logging.h>
+#include <android-base/properties.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
 
-#define SERIAL_NUMBER_FILE "/efs/FactoryApp/serial_no"
+#include "init_msm8916.h"
 
 using android::base::GetProperty;
-using android::base::ReadFileToString;
-using android::base::Trim;
 using android::init::property_set;
-
-void property_override(char const prop[], char const value[])
-{
-    prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
-{
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
-}
-
-/* Read the file at filename and returns the integer
- * value in the file.
- *
- * @prereq: Assumes that integer is non-negative.
- *
- * @return: integer value read if succesful, -1 otherwise. */
-int read_integer(const char* filename)
-{
-	int retval;
-	FILE * file;
-
-	/* open the file */
-	if (!(file = fopen(filename, "r"))) {
-		return -1;
-	}
-	/* read the value from the file */
-	fscanf(file, "%d", &retval);
-	fclose(file);
-
-	return retval;
-}
-
-void set_fingerprint()
-{
-	std::string fingerprint = GetProperty("ro.build.fingerprint", "");
-
-	if ((strlen(fingerprint.c_str()) > 1) && (strlen(fingerprint.c_str()) <= PROP_VALUE_MAX))
-		return;
-
-	char new_fingerprint[PROP_VALUE_MAX+1];
-
-	std::string build_id = GetProperty("ro.build.id","");
-	std::string build_tags = GetProperty("ro.build.tags","");
-	std::string build_type = GetProperty("ro.build.type","");
-	std::string device = GetProperty("ro.product.device","");
-	std::string incremental_version = GetProperty("ro.build.version.incremental","");
-	std::string release_version = GetProperty("ro.build.version.release","");
-
-	snprintf(new_fingerprint, PROP_VALUE_MAX, "samsung/%s/%s:%s/%s/%s:%s/%s",
-		device.c_str(), device.c_str(), release_version.c_str(), build_id.c_str(),
-		incremental_version.c_str(), build_type.c_str(), build_tags.c_str());
-
-	property_override_dual("ro.build.fingerprint", "ro.boot.fingerprint", new_fingerprint);
-}
-
-void set_cdma_properties()
-{
-	android::init::property_set("ro.cdma.home.operator.alpha", "Chameleon");
-	android::init::property_set("ro.cdma.home.operator.numeric", "310000");
-	android::init::property_set("ro.telephony.default_network", "10");
-}
-
-void set_gsm_properties()
-{
-	android::init::property_set("telephony.lteOnCdmaDevice", "0");
-	android::init::property_set("ro.telephony.default_network", "9");
-}
-
-void set_wifi_properties()
-{
-    android::init::property_set("ro.carrier", "wifi-only");
-    android::init::property_set("ro.radio.noril", "1");
-}
-
-void set_target_properties(const char *device, const char *model)
-{
-	property_override_dual("ro.product.device", "ro.product.vendor.device", device);
-	property_override_dual("ro.product.model", "ro.product.vendor.model", model);
-
-	/* check and/or set fingerprint */
-	set_fingerprint();
-
-	char const *serial_number_file = SERIAL_NUMBER_FILE;
-	std::string serial_number;
-
-	if (ReadFileToString(serial_number_file, &serial_number)) {
-        	serial_number = Trim(serial_number);
-        	property_override("ro.serialno", serial_number.c_str());
-	}
-}
 
 void vendor_load_properties()
 {
-	char *device = NULL;
-	char *model = NULL;
-
 	/* get the bootloader string */
 	std::string bootloader = android::base::GetProperty("ro.bootloader", "");
 
 	if (bootloader.find("T377P") == 0) {
-		device = (char *)"gtesqltespr";
-		model = (char *)"SM-T377P";
-		set_cdma_properties();
-	}
-	else if (bootloader.find("T560NUU") == 0) {
-		device = (char *)"gtelwifiue";
-		model = (char *)"SM-T560NU";
-		set_wifi_properties();
-	}
-	else if (bootloader.find("T550") == 0) {
-		device = (char *)"gt510wifi";
-		model = (char *)"SM-T550";
-		set_wifi_properties();
-	}
-	else if (bootloader.find("T350") == 0) {
-		device = (char *)"gt58wifi";
-		model = (char *)"SM-T350";
-		set_wifi_properties();
-	}
-	else if (bootloader.find("T357T") == 0) {
-		device = (char *)"gt58ltetmo";
-		model = (char *)"SM-T357T";
-		set_gsm_properties();
-	}
-	else {
+		/* gtesqltespr */
+		property_override("ro.build.description", "gtesqltespr-user 7.1.1 NMF26X T377PVPU3CQI4 release-keys");
+        set_ro_product_prop("device", "gtesqltespr");
+        set_ro_build_prop("fingerprint", "samsung/gtesqltespr/gtesqltespr:7.1.1/NMF26X/T377PVPU3CQI4:user/release-keys");
+        set_ro_product_prop("model", "SM-T377P");
+        set_ro_product_prop("name", "gtesqltespr");	
+		cdma_properties("Default", "310000", "0", "10");
+	} else if (bootloader.find("T560NUU") == 0) {
+		/* gtelwifiue */
+        property_override("ro.build.description", "gtelwifiue-user 7.1.1 NMF26X T560NUUEU1CRG1 release-keys");
+        set_ro_product_prop("device", "gtelwifiue");
+		set_ro_build_prop("fingerprint", "samsung/samsung/gtelwifiue/gtelwifiue:7.1.1/NMF26X/T560NUUEU1CRG1:user/release-keys");
+        set_ro_product_prop("model", "SM-T560NU");
+        set_ro_product_prop("name", "gtelwifiue");	
+		wifi_properties("wifi-only", "1");
+	} else {
 		return;
 	}
 
-	/* set the properties */
-	set_target_properties(device, model);
+	std::string device = GetProperty("ro.product.device", "");
+    LOG(ERROR) << "Found bootloader id " << bootloader <<  " setting build properties for "
+    << device <<  " device" << std::endl;
 }
